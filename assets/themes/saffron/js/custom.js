6813855b3ab4c5befa536d32a7ff3a7d57aca464
Rob Easthope <robeasthope@gmail.com>
2013-04-27 15:56:35 +0100

    Fullscreen jumbotron complete

--- assets/themes/saffron/js/custom.js
+++ assets/themes/saffron/js/custom.js
@@ -1,32 +1,30 @@
-$(document).ready(function() {
+function oknFullScreen() {
 	if ($(window).height() > $(".jumbotron-content-wrap").height()) {
+		// Enlarge jumbotron to fullscreen
+		$(".jumbotron").css("min-width",function(){
+			return $(window).width();
+		});
+
 		$(".jumbotron").css("min-height",function(){
 			return $(window).height();
 		});
 
+		// Center jumbotron content vertically
 		$(".jumbotron-content-wrap").css("margin-top",function(){
 			return ($(".jumbotron").height() - $(".jumbotron-content-wrap").height()) / 2;
 		});
 
-		$(".index-padding").css("padding-bottom",function(){
-			return $(window).height();
+		// Center jumbotron on screen
+		$(".jumbotron").css("margin-left",function(){
+			return -(($(window).width() - $(".col-span-12").width()) / 2);
 		});
 	}
+};
+
+$(document).ready(function() {
+	oknFullScreen();
 });
 
 $(window).resize(function() {
-	if ($(window).height() > $(".jumbotron-content-wrap").height()) {
-		$(".jumbotron").css("min-height",function(){
-			return $(window).height();
-		});
-
-		$(".jumbotron-content-wrap").css("margin-top",function(){
-			return ($(".jumbotron").height() - $(".jumbotron-content-wrap").height()) / 2;
-		});
-		
-		$(".index-padding").css("padding-bottom",function(){
-			return $(window).height();
-		});
-
-	}
+	oknFullScreen();
 });
