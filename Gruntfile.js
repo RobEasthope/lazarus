4cb25455dbac7f2e6c1297fea7a880507f37044d
Rob Easthope <robeasthope@gmail.com>
2013-06-25 20:04:10 +0100

    Grunt build changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,10 @@ module.exports = function(grunt) {
     recess: {
       dist: {
         options: {
-          compile: true,
-          compress: true
+          compile: true
         },
         files: {
           'assets/themes/saffron/css/main.min.css': [
-            'assets/themes/saffron/css/less/bootstrap/bootstrap.less',
-            'assets/themes/saffron/css/less/retina/retina.less',
             'assets/themes/saffron/css/less/app.less'
           ]
         }
@@ -77,7 +74,7 @@ module.exports = function(grunt) {
   });
 
   // Load tasks
-  //grunt.loadTasks('tasks');
+  grunt.loadTasks('tasks');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
