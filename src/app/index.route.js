954b34245682de410257ebf5aab3ea6281850b12
Rob Easthope <robeasthope@gmail.com>
2015-08-13 10:14:18 +0100

    Add routing for each project

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -6,6 +6,48 @@ function routerConfig ($stateProvider, $urlRouterProvider) {
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
+    })
+    .state('ambr', {
+      url: '/ambr',
+      templateUrl: 'app/projects/ambr.html',
+      controller: 'AmbrController',
+      controllerAs: 'ambr'
+    })
+    .state('brixton', {
+      url: '/brixton',
+      templateUrl: 'app/projects/brixton.html',
+      controller: 'BrixtonController',
+      controllerAs: 'brixton'
+    })
+    .state('eufn', {
+      url: '/eufn',
+      templateUrl: 'app/projects/eufn.html',
+      controller: 'EufnController',
+      controllerAs: 'eufn'
+    })
+    .state('lambeth', {
+      url: '/lambeth',
+      templateUrl: 'app/projects/lambeth.html',
+      controller: 'LambethController',
+      controllerAs: 'lambeth'
+    })
+    .state('lancastrians', {
+      url: '/lancastrians',
+      templateUrl: 'app/projects/lancastrians.html',
+      controller: 'LancastriansController',
+      controllerAs: 'lancastrians'
+    })
+    .state('layla', {
+      url: '/layla',
+      templateUrl: 'app/projects/layla.html',
+      controller: 'LaylaController',
+      controllerAs: 'layla'
+    })
+    .state('saffron', {
+      url: '/saffron',
+      templateUrl: 'app/projects/saffron.html',
+      controller: 'SaffronController',
+      controllerAs: 'saffron'
     });
 
   $urlRouterProvider.otherwise('/');
