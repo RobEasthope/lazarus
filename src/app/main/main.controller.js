dbfbd187ddc09328e18c71fdacdaa1643d662959
Rob Easthope <robeasthope@gmail.com>
2015-08-14 10:52:38 +0100

    Remove animated content on main page

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,17 +3,12 @@ class MainController {
     'ngInject';
 
     this.awesomeThings = [];
-    this.classAnimation = '';
-    this.toastr = toastr;
 
     this.activate($timeout, webDevTec);
   }
 
   activate($timeout, webDevTec) {
     this.getWebDevTec(webDevTec);
-    $timeout(() => {
-      this.classAnimation = 'rubberBand';
-    }, 4000);
   }
 
   getWebDevTec(webDevTec) {
@@ -22,11 +17,6 @@ class MainController {
     angular.forEach(this.awesomeThings, (awesomeThing) => {
       awesomeThing.rank = Math.random();
     });
-  }
-
-  showToastr() {
-    this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-    this.classAnimation = '';
   }
 }
 
