b5914fa53a4c76beb26cc6f8a666db556112ca67
Rob Easthope <robeasthope@gmail.com>
2015-09-28 12:57:28 +0100

    Commenting

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -7,9 +7,10 @@ import runBlock from './index.run';
 import MainController from './main/main.controller';
 
 angular.module('lazarus', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router'])
-
+  // General config
   .config(config)
 
+  // Routing
   .config(routerConfig)
 
   .run(runBlock)
