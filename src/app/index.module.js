0b8399faf0d56090c1c7eac201ab8ef5bbaa4fcb
Rob Easthope <robeasthope@gmail.com>
2015-09-28 12:50:24 +0100

    Strip out gulp-angular template code

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,26 +1,16 @@
-/* global malarkey:false, toastr:false, moment:false */
+/* global*/
 import config from './index.config';
 
 import routerConfig from './index.route';
 
 import runBlock from './index.run';
 import MainController from './main/main.controller';
-import GithubContributorService from '../app/components/githubContributor/githubContributor.service';
-import WebDevTecService from '../app/components/webDevTec/webDevTec.service';
-import NavbarDirective from '../app/components/navbar/navbar.directive';
-import MalarkeyDirective from '../app/components/malarkey/malarkey.directive';
 
 angular.module('lazarus', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router'])
-  .constant('malarkey', malarkey)
-  .constant('toastr', toastr)
-  .constant('moment', moment)
+
   .config(config)
 
   .config(routerConfig)
 
   .run(runBlock)
-  .service('githubContributor', GithubContributorService)
-  .service('webDevTec', WebDevTecService)
-  .controller('MainController', MainController)
-  .directive('acmeNavbar', () => new NavbarDirective())
-  .directive('acmeMalarkey', () => new MalarkeyDirective(malarkey));
+  .controller('MainController', MainController);
