a1e9c76cb3771df50794c6ecb3c9e8841b6081cc
Rob Easthope <robeasthope@gmail.com>
2015-09-28 17:50:57 +0100

    Fix AWS task

--- gulp/aws-s3.js
+++ gulp/aws-s3.js
@@ -2,11 +2,14 @@
 
 var gulp = require('gulp');
 var s3 = require("gulp-s3");
+var fs = require('fs');
 
 // Call aws.json file
-aws = JSON.parse(fs.readFileSync('./aws.json'));
+var aws = JSON.parse(fs.readFileSync('./aws.json'));
 
 gulp.task('aws-assets', function(done) {
+  // aws = JSON.parse(fs.readFileSync('./aws.json'));
+
   gulp.src('./src/assets/**/*.*')
     .pipe(s3(aws));
 });
