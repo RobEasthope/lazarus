d3b6833d579be04b9827084e1c81a2f50e729298
Rob Easthope <robeasthope@gmail.com>
2015-10-09 13:05:16 +0100

    Working path replacement task

--- build/gulp/deploy-code.js
+++ build/gulp/deploy-code.js
@@ -1,8 +1,30 @@
 'use strict';
 
 var gulp = require('gulp');
+var replace = require('gulp-replace-task');
 
-gulp.task('deploy', ['build'], function(done) {
+gulp.task('deploy-site', ['build'], function(done) {
   gulp.src('./dist/**/*.*')
     .pipe(gulp.dest('./../'));
+});
+
+'use strict';
+
+
+// *
+
+// Deploy code
+gulp.task('paths', function(done) {
+  gulp.src('./dist/**/*.js')
+  .pipe(replace({
+      patterns: [
+        {
+          match: /assets/g,
+          replacement: function () {
+            return 'build/dist/assets'; // replaces "foo" to "bar"
+          }
+        }
+      ]
+    }))
+  .pipe(gulp.dest('./test'));
 });
