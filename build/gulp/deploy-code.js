ff00aedce11cf49e97c10dfb7b272c0b4dbd8974
Rob Easthope <robeasthope@gmail.com>
2015-10-09 15:27:30 +0100

    Update deployment task for AWS deployment

--- build/gulp/deploy-code.js
+++ build/gulp/deploy-code.js
@@ -3,28 +3,83 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace-task');
 
-gulp.task('deploy-site', ['build'], function(done) {
-  gulp.src('./dist/**/*.*')
-    .pipe(gulp.dest('./../'));
-});
+// AWS reqs
+var s3 = require("gulp-s3");
+var fs = require('fs');
 
-'use strict';
+// Call aws.json file
+var aws = JSON.parse(fs.readFileSync('./aws.json'));
 
 
-// *
+gulp.task('clean', require('del').bind(null, ['dist']));
 
-// Deploy code
-gulp.task('paths', function(done) {
-  gulp.src('./dist/**/*.js')
-  .pipe(replace({
+// Update asset paths in JS files
+gulp.task('assets-paths', function(done) {
+  gulp.src('./dist/scripts/*.js')
+    // Rewrite assets paths to AWS CDN
+    .pipe(replace({
       patterns: [
         {
           match: /assets/g,
           replacement: function () {
-            return 'build/dist/assets'; // replaces "foo" to "bar"
+            return 'https://s3-eu-west-1.amazonaws.com/robeasthope.com'; // replaces "foo" to "bar"
           }
         }
       ]
     }))
-  .pipe(gulp.dest('./test'));
+    .pipe(gulp.dest('./dist/scripts/*.js'));
+});
+
+
+// Update asset, JS, and CSS paths
+gulp.task('deploy-index', function(done) {
+  gulp.src(['./dist/index.html', ''])
+    // Replace CSS paths
+    .pipe(replace({
+      patterns: [
+        {
+          match: /styles/g,
+          replacement: function () {
+            return 'https://s3-eu-west-1.amazonaws.com/robeasthope.com/styles';
+          }
+        }
+      ]
+    }))
+
+    // Replace JS paths
+    .pipe(replace({
+      patterns: [
+        {
+          match: /scripts/g,
+          replacement: function () {
+            return 'https://s3-eu-west-1.amazonaws.com/robeasthope.com/scripts';
+          }
+        }
+      ]
+    }))
+
+    // Rewrite assets paths to AWS CDN
+    .pipe(replace({
+      patterns: [
+        {
+          match: /assets/g,
+          replacement: function () {
+            return 'https://s3-eu-west-1.amazonaws.com/robeasthope.com';
+          }
+        }
+      ]
+    }))
+  .pipe(gulp.dest('./'));
+});
+
+
+// Deploy assets to AWS
+gulp.task('aws-assets', function(done) {
+  gulp.src(['./dist/**/*.*'])
+    .pipe(s3(aws));
+});
+
+// Deploy task
+gulp.task('deploy', ['clean', 'build', 'asset-paths', 'aws-assets', 'deploy-index' ], function(done) {
+
 });
