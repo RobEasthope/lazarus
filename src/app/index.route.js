b3fd8094f06372b29aebd0f89965e868f083bc66
Rob Easthope <robeasthope@gmail.com>
2015-10-15 13:53:51 +0100

    Fix routing

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -31,135 +31,14 @@ function routerConfig ($stateProvider, $urlRouterProvider) {
           templateUrl: 'app/projects/ambr/ambr.html'
         }
       }
-    }),
-    .state('layla', {
+    })
+    .state('layla-moran', {
       url: '/layla-moran',
       views: {
         'content': {
           templateUrl: 'app/projects/layla/layla.html'
         }
       }
-    // }),
-    // .state('eufn', {
-    //   url: '/EUFN',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/eufn/eufn.html'
-    //     }
-    //   }
-    // }),
-    // .state('sony-wheel', {
-    //   url: '/sony-wheel',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // }),
-    // .state('gulp-emails', {
-    //   url: '/ambr',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // }),
-    // .state('haygarth', {
-    //   url: '/ambr',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // }),
-    // .state('haygarth-digital-room', {
-    //   url: '/ambr',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // }),
-    // .state('lambeth-advice-centre', {
-    //   url: '/ambr',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // }),
-    // .state('lancastrians', {
-    //   url: '/ambr',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // }),
-    // .state('sony-demo-bay', {
-    //   url: '/ambr',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // }),
-    // .state('think-differently', {
-    //   url: '/ambr',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // }),
-    // .state('waterleaf', {
-    //   url: '/ambr',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // }),
-    // .state('ysl-rock-chick', {
-    //   url: '/ambr',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // }),
-    // .state('giles-deacon', {
-    //   url: '/giles-deacon',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // }),
-    // .state('toni-and-guy', {
-    //   url: '/toni-and-guy',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // }),
-    // .state('dumfries-house', {
-    //   url: '/dumfries-house',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // })
-    // .state('louise-and-daniel', {
-    //   url: '/louise-and-daniel',
-    //   views: {
-    //     'content': {
-    //       templateUrl: 'app/projects/ambr/ambr.html'
-    //     }
-    //   }
-    // })
     });
 
   $urlRouterProvider.otherwise('/');
