d5716a5b95e96773d630bcdfd886d24b3a3a759a
Rob Easthope <robeasthope@gmail.com>
2015-10-19 12:23:28 +0100

    Comment out non-essential portfolio pieces for the time being

--- src/app/home/portfolio-index.service.js
+++ src/app/home/portfolio-index.service.js
@@ -13,11 +13,11 @@ class PortfolioIndexService {
       	image : 'assets/images/projects/haygarth-digital-room/haygarth-digital-room-thumbnail.jpg',
         state  : 'haygarth-digital-room'
       },
-      {
-        title : 'Think Differently',
-      	image : 'assets/images/projects/think-differently/think-differently-thumbnail.jpg',
-        state  : 'think-differently'
-      },
+      // {
+      //   title : 'Think Differently',
+      // 	image : 'assets/images/projects/think-differently/think-differently-thumbnail.jpg',
+      //   state  : 'think-differently'
+      // },
       {
         title : 'YSL Rock Chick',
       	image : 'assets/images/projects/ysl-rock-chick/ysl-rock-chick-thumbnail.jpg',
@@ -43,26 +43,26 @@ class PortfolioIndexService {
       	image : 'assets/images/projects/giles-deacon/giles-deacon-thumbnail.jpg',
         state  : 'giles-deacon'
       },
-      {
-        title : 'Toni & Guy',
-      	image : 'assets/images/projects/toni-and-guy/toni-and-guy-thumbnail.png',
-        state  : 'toni-and-guy'
-      },
-      {
-        title : 'Louise & Daniel',
-      	image : 'assets/images/projects/louise-and-daniel/louise-and-daniel-thumbnail.jpg',
-        state  : 'louise-and-daniel'
-      },
-      {
-        title : 'Dumfries House',
-      	image : 'assets/images/projects/dumfries-house/dumfries-house-thumbnail.jpg',
-        state  : 'dumfries-house'
-      },
-      {
-        title : 'Lambeth Advice Network',
-      	image : 'assets/images/projects/lambeth/lambeth-thumbnail.jpg',
-        state  : 'lambeth'
-      },
+      // {
+      //   title : 'Toni & Guy',
+      // 	image : 'assets/images/projects/toni-and-guy/toni-and-guy-thumbnail.png',
+      //   state  : 'toni-and-guy'
+      // },
+      // {
+      //   title : 'Louise & Daniel',
+      // 	image : 'assets/images/projects/louise-and-daniel/louise-and-daniel-thumbnail.jpg',
+      //   state  : 'louise-and-daniel'
+      // },
+      // {
+      //   title : 'Dumfries House',
+      // 	image : 'assets/images/projects/dumfries-house/dumfries-house-thumbnail.jpg',
+      //   state  : 'dumfries-house'
+      // },
+      // {
+      //   title : 'Lambeth Advice Network',
+      // 	image : 'assets/images/projects/lambeth/lambeth-thumbnail.jpg',
+      //   state  : 'lambeth'
+      // },
       {
         title : 'AMBR Media',
       	image : 'assets/images/projects/ambr/ambr-thumbnail.png',
@@ -73,11 +73,11 @@ class PortfolioIndexService {
       	image : 'assets/images/projects/layla/layla-thumbnail.png',
         state  : 'layla-moran'
       },
-      {
-        title : 'Lancastrians',
-      	image : 'assets/images/projects/lancastrians/lancastrians-thumbnail.jpg',
-        state  : 'lancastrians'
-      },
+      // {
+      //   title : 'Lancastrians',
+      // 	image : 'assets/images/projects/lancastrians/lancastrians-thumbnail.jpg',
+      //   state  : 'lancastrians'
+      // },
       {
         title : 'EUFN',
       	image : 'assets/images/projects/eufn/eufn-thumbnail.png',
