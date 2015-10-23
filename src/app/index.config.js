a9e60d80d569aa5a2545f3d40864ad18767b4cb6
Rob Easthope <robeasthope@gmail.com>
2015-10-23 02:18:14 +0100

    Clean up angular config

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -2,12 +2,6 @@ function config ($logProvider) {
   'ngInject';
   // Enable log
   $logProvider.debugEnabled(true);
-
-  // Set options third-party lib
-  // toastr.options.timeOut = 3000;
-  // toastr.options.positionClass = 'toast-top-right';
-  // toastr.options.preventDuplicates = true;
-  // toastr.options.progressBar = true;
 }
 
 export default config;
