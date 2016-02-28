5f7d2c7038db473dddd0e7c2a55520fc32fab9a5
Rob Easthope <robeasthope@gmail.com>
2016-02-28 19:22:22 +0000

    Restore blank aws.json for testing

--- gulp/aws-s3.js
+++ gulp/aws-s3.js
@@ -5,7 +5,7 @@ var s3 = require("gulp-s3");
 var fs = require('fs');
 
 // Call aws.json file
-// var aws = JSON.parse(fs.readFileSync('./aws.json'));
+var aws = JSON.parse(fs.readFileSync('./aws.json'));
 
 gulp.task('aws-assets', function(done) {
   gulp.src('./src/assets/**/*.*')
