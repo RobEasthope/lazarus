64ffd76107741f8894faf91f3de1d2fc54682bbf
Rob Easthope <robeasthope@gmail.com>
2016-02-28 13:44:13 +0000

    Trim out old demo code

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/dist'));
 
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('dist');
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
