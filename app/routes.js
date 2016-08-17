91a4476030ff65ec01c044d7a68bbfa1a4c05bb1
Rob Easthope <hello@robeasthope.com>
2016-08-17 16:01:56 +0100

    Add projects routing

--- app/routes.js
+++ app/routes.js
@@ -34,6 +34,22 @@ export default function createRoutes(store) {
         importModules.catch(errorLoading);
       },
     }, {
+      path: '/projects',
+      name: 'projects',
+      getComponent(nextState, cb) {
+        const importModules = Promise.all([
+          System.import('containers/Projects'),
+        ]);
+
+        const renderRoute = loadModule(cb);
+
+        importModules.then(([component]) => {
+          renderRoute(component);
+        });
+
+        importModules.catch(errorLoading);
+      },
+    }, {
       path: '*',
       name: 'notfound',
       getComponent(nextState, cb) {
