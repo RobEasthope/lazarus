ea3753b43b15bd1d511733fe117e1da997b1793f
Rob Easthope <hello@robeasthope.com>
2016-09-06 12:00:43 +0100

    Duplicate Projects container

---
+++ app/containers/Project/index.js
@@ -0,0 +1,20 @@
+/*
+ * Projects
+ */
+
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import ReactDOM from 'react-dom';
+import ReactMarkdown from 'react-markdown';
+import styles from './styles.css';
+
+var input = require('./project-index.md');
+
+export default class Projects extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+  render() {
+    return (
+			<ReactMarkdown source={input} />
+    );
+  }
+}
