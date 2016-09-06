0c4a58f943a66a3583cd9c0f343844c038c1458b
Rob Easthope <hello@robeasthope.com>
2016-09-06 12:01:28 +0100

    Remove markdown content from Projects

--- app/containers/Projects/index.js
+++ app/containers/Projects/index.js
@@ -5,16 +5,14 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import ReactDOM from 'react-dom';
-import ReactMarkdown from 'react-markdown';
 import styles from './styles.css';
 
-var input = require('./project-index.md');
 
 export default class Projects extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   render() {
     return (
-			<ReactMarkdown source={input} />
+      <h1>PROJECT INDEX</h1>
     );
   }
 }
